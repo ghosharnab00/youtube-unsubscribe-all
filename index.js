a02ff4a7a95fe667c682f072ac5e53530bcec9b1
Arnab g <ag@Arnabs-MacBook-Air.local>
2023-10-07 01:47:17 +0530

    added background js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const start = document.getElementById('start')
+const channelsList = document.getElementById('channels-list');
 
 start.addEventListener('click', () => {
     const youtubeUrl = "https://www.youtube.com/feed/channels";
@@ -8,9 +9,10 @@ start.addEventListener('click', () => {
 
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
         const activeTab = tabs[0];
-        console.log(activeTab)
         if (activeTab.url === youtubeUrl) {
             start.innerText = 'Already on the correct page.';
+            scrapeChannels(activeTab.id);
+
         } else {
             start.innerText = "redirect to youtube ..";
           chrome.tabs.update(activeTab.id, { url: youtubeUrl });
@@ -20,3 +22,11 @@ start.addEventListener('click', () => {
       });
 })
 
+
+function scrapeChannels(tabId) {
+  chrome.tabs.executeScript(tabId, { code: `
+       console.log("channels")    
+  `});
+}
+
+// const channels = Array.from(document.querySelectorAll('.style-scope.ytd-channel-renderer'));
